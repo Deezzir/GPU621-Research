d4effad02d758bfc3b10c6657305717f9a3bb47f
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2021-12-08 23:00:34 -0500

    Update TBB_Main.cpp

--- TBB_Main.cpp
+++ TBB_Main.cpp
@@ -1,15 +1,57 @@
-// Workshop 7 - Prefix Scan Comparison
- // w7.tbb.cpp
- // 2018.11.04
- // Chris Szalwinski
+// Iurii Kondrakov
+// TBB_Main.cpp
+// 2021.12.07
 
 #include <iostream>
 #include <chrono>
 #include <tbb/tbb.h>
-#include "Body.h"
+#include <tbb/parallel_reduce.h>
+
+template<typename T, typename C>
+class Body {
+    T accumul_;
+    const T* const in_;
+    T* const out_;
+    const T identity_;
+    const C combine_;
+
+public:
+    Body(T* out, const T* in, T i, C c) :
+        accumul_(i), identity_(i),
+        in_(in), out_(out),
+        combine_(c) {}
+
+    Body(Body& src, tbb::split) :
+        accumul_(src.identity_), identity_(src.identity_),
+        in_(src.in_), out_(src.out_),
+        combine_(src.combine_) {}
+
+    template<typename Tag>
+    void operator() (const tbb::blocked_range<T>& r, Tag) {
+        T temp = accumul_;
+        for (auto i = r.begin(); i < r.end(); i++) {
+            if (Tag::is_final_scan())
+                out_[i] = temp;
+            temp = combine_(temp, in_[i]);
+        }
+        accumul_ = temp;
+    }
+
+    T get_accumul() {
+        return accumul_;
+    }
+
+    void reverse_join(Body& src) {
+        accumul_ = combine_(accumul_, src.accumul_);
+    }
+
+    void assign(Body& src) {
+        accumul_ = src.accumul_;
+    }
+};
+
 
 // report system time
-//
 void reportTime(const char* msg, std::chrono::steady_clock::duration span) {
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(span);
     std::cout << msg << " - took - " <<
@@ -36,8 +78,8 @@ int main(int argc, char** argv) {
     }
 
     // initial values for testing
-    const int N = 8;
-    const int in_[N]{ 3, 1, 7, 0, 1, 4, 5, 9 };
+    const int N = 9;
+    const int in_[N]{ 3, 1, 7, 0, 1, 4, 5, 9, 2 };
 
     // command line arguments - none for testing, 1 for large arrays
     int n;
@@ -67,11 +109,10 @@ int main(int argc, char** argv) {
     else
         tbb::parallel_scan(tbb::blocked_range<int>(0, n), body);
     te = std::chrono::steady_clock::now();
-    std::cout << "n = " << n << ": ";
     for (int i = 0; i < N; i++)
         std::cout << out[i] << ' ';
     std::cout << out[n - 1] << std::endl;
-    reportTime("Inclusive Prefix Scan", te - ts);
+    reportTime("Exclusive Scan", te - ts);
 
     delete[] in;
     delete[] out;
